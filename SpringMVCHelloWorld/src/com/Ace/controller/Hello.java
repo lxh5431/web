089fe9ee2373b8bd9bec8f4740027650939ef744
lxh5431 <lxh5431@gmail.com>
2016-07-24 00:47:20 +0800

    简单的springmvc入门

---
+++ SpringMVCHelloWorld/src/com/Ace/controller/Hello.java
@@ -0,0 +1,64 @@
+package com.Ace.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class Hello {
+	  //hello world
+
+    @RequestMapping(value="/hello")
+
+    public String hello(){
+
+        System.out.println("spring mvc hello world!");
+
+        return "hello";
+
+    }
+
+   
+
+    //hello world
+
+    @RequestMapping(value="/ok")
+
+    @ResponseBody
+
+    public Object ok(){
+
+        System.out.println("ok");
+
+        List<String> list=new ArrayList<String>(); 
+
+        list.add("电视机"); 
+
+        list.add("冰箱"); 
+
+        list.add("山东省"); 
+
+        list.add("多发点"); 
+
+        list.add("D大调"); 
+
+        list.add("规范"); 
+
+        list.add("啦啦啦"); 
+
+        list.add("咯就是"); 
+
+        list.add("阿瓦尔"); 
+
+        return list; 
+
+    }
+
+     
+
+
+
+}
+
+
